37a08e4d9fe314d5fbe672d78e76166c419bae76
Jeferson Echavarria <jfrson24@gmail.com>
2024-07-03 08:23:40 -0500

    A menu with the following items is added, File, select, commands, view, network, configuration and start.

--- File_Manager/File_Manager/Form1.Designer.cs
+++ File_Manager/File_Manager/Form1.Designer.cs
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            menu = new MenuStrip();
+            archivoToolStripMenuItem = new ToolStripMenuItem();
+            seleccionarToolStripMenuItem = new ToolStripMenuItem();
+            comandoToolStripMenuItem = new ToolStripMenuItem();
+            redToolStripMenuItem = new ToolStripMenuItem();
+            verToolStripMenuItem = new ToolStripMenuItem();
+            configuraciónToolStripMenuItem = new ToolStripMenuItem();
+            temaToolStripMenuItem = new ToolStripMenuItem();
+            oscuroToolStripMenuItem = new ToolStripMenuItem();
+            claroToolStripMenuItem = new ToolStripMenuItem();
+            inicioToolStripMenuItem = new ToolStripMenuItem();
+            menu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menu
+            // 
+            menu.Items.AddRange(new ToolStripItem[] { archivoToolStripMenuItem, seleccionarToolStripMenuItem, comandoToolStripMenuItem, redToolStripMenuItem, verToolStripMenuItem, configuraciónToolStripMenuItem, inicioToolStripMenuItem });
+            menu.Location = new Point(0, 0);
+            menu.Name = "menu";
+            menu.Size = new Size(909, 24);
+            menu.TabIndex = 0;
+            menu.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            archivoToolStripMenuItem.Size = new Size(60, 20);
+            archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // seleccionarToolStripMenuItem
+            // 
+            seleccionarToolStripMenuItem.Name = "seleccionarToolStripMenuItem";
+            seleccionarToolStripMenuItem.Size = new Size(79, 20);
+            seleccionarToolStripMenuItem.Text = "Seleccionar";
+            // 
+            // comandoToolStripMenuItem
+            // 
+            comandoToolStripMenuItem.Name = "comandoToolStripMenuItem";
+            comandoToolStripMenuItem.Size = new Size(72, 20);
+            comandoToolStripMenuItem.Text = "Comando";
+            // 
+            // redToolStripMenuItem
+            // 
+            redToolStripMenuItem.Name = "redToolStripMenuItem";
+            redToolStripMenuItem.Size = new Size(39, 20);
+            redToolStripMenuItem.Text = "Red";
+            // 
+            // verToolStripMenuItem
+            // 
+            verToolStripMenuItem.Name = "verToolStripMenuItem";
+            verToolStripMenuItem.Size = new Size(35, 20);
+            verToolStripMenuItem.Text = "Ver";
+            // 
+            // configuraciónToolStripMenuItem
+            // 
+            configuraciónToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { temaToolStripMenuItem });
+            configuraciónToolStripMenuItem.Name = "configuraciónToolStripMenuItem";
+            configuraciónToolStripMenuItem.Size = new Size(95, 20);
+            configuraciónToolStripMenuItem.Text = "Configuración";
+            // 
+            // temaToolStripMenuItem
+            // 
+            temaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { oscuroToolStripMenuItem, claroToolStripMenuItem });
+            temaToolStripMenuItem.Name = "temaToolStripMenuItem";
+            temaToolStripMenuItem.Size = new Size(180, 22);
+            temaToolStripMenuItem.Text = "Tema";
+            // 
+            // oscuroToolStripMenuItem
+            // 
+            oscuroToolStripMenuItem.Name = "oscuroToolStripMenuItem";
+            oscuroToolStripMenuItem.Size = new Size(180, 22);
+            oscuroToolStripMenuItem.Text = "Oscuro";
+            // 
+            // claroToolStripMenuItem
+            // 
+            claroToolStripMenuItem.Name = "claroToolStripMenuItem";
+            claroToolStripMenuItem.Size = new Size(180, 22);
+            claroToolStripMenuItem.Text = "Claro";
+            // 
+            // inicioToolStripMenuItem
+            // 
+            inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
+            inicioToolStripMenuItem.Size = new Size(48, 20);
+            inicioToolStripMenuItem.Text = "Inicio";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(909, 587);
+            Controls.Add(menu);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            menu.ResumeLayout(false);
+            menu.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menu;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem seleccionarToolStripMenuItem;
+        private ToolStripMenuItem comandoToolStripMenuItem;
+        private ToolStripMenuItem redToolStripMenuItem;
+        private ToolStripMenuItem verToolStripMenuItem;
+        private ToolStripMenuItem configuraciónToolStripMenuItem;
+        private ToolStripMenuItem temaToolStripMenuItem;
+        private ToolStripMenuItem oscuroToolStripMenuItem;
+        private ToolStripMenuItem claroToolStripMenuItem;
+        private ToolStripMenuItem inicioToolStripMenuItem;
     }
 }
